5653a59dc72402def4b0f6e6c535331c64a2c6c8
Arthur Morton <a.smorton5@gmail.com>
2022-04-19 00:32:40 +0100

    InitialCamera module

---
+++ src/StarterPlayer/StarterCharacterScripts/InitialCamera.client.lua
@@ -0,0 +1,5 @@
+-- When a player gets a new character, set their camera to the kit selection cam
+local Camera = workspace.CurrentCamera
+
+--[[Camera.CameraType = Enum.CameraType.Scriptable
+Camera.CFrame = CFrame.new(0, 0, 0)]]

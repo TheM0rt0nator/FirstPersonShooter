f5977dc039fcdc4c44b8bba6fac62ca3937f2376
Arthur Morton <a.smorton5@gmail.com>
2022-04-19 00:31:03 +0100

    MainGameLoop!

---
+++ src/ServerScriptService/ServerScripts/MainGameLoop/init.lua
@@ -0,0 +1,90 @@
+-- Runs the main game loop
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local loadModule, getDataStream = table.unpack(require(ReplicatedStorage.Framework))
+
+local MapHandler = loadModule("MapHandler")
+local Gamemodes = loadModule("Gamemodes")
+local Maths = loadModule("Maths")
+local Scoreboard = loadModule("Scoreboard")
+
+local spawnPlayerEvent = getDataStream("SpawnPlayerEvent", "RemoteEvent")
+local gameOverEvent = getDataStream("GameOverEvent", "BindableEvent")
+
+local MainGameLoop = {}
+
+-- Starts the main game loop which loads/unloads maps and starts/ends rounds (the initiate function is called when the module is loaded in ModuleScriptLoader)
+function MainGameLoop:initiate()
+	self:createGameValues()
+	self.gameRunning = true
+	task.spawn(function()
+		while self.gameRunning do
+			task.wait(1)
+			self.gameStatus.Value = "ChoosingMap"
+			-- Choose the map
+			local mapLoaded, err = pcall(function()
+				MapHandler:chooseMap()
+			end)
+			if not mapLoaded then 
+				-- If the maps fails to load, start a new round and hope it loads next time
+				warn(err)
+				continue
+			end
+
+			self.gameStatus.Value = "ChoosingGamemode"
+			local modeChosen, err = pcall(function()
+				self.currentMode = Gamemodes:chooseGamemode()
+				self.roundType.Value = self.currentMode.roundType or "Kills"
+			end)
+			if not modeChosen then 
+				-- If the gamemode fails to be chosen, start a new round
+				warn(err)
+				continue
+			end
+
+			-- Do game stuff
+			gameOverEvent.Event:Wait()
+
+			-- Tell all players to display the winners, then if anyone new joins, they can just wait for gameStatus to change back to choosing map
+			self.gameStatus.Value = "GameOver"
+			self.currentMode = nil
+
+			Scoreboard:clearScores()
+			-- Unload the map
+			local mapUnloaded, err = pcall(function()
+				MapHandler:unloadMap()
+			end)
+			if not mapUnloaded then
+				-- If this function fails, there can't be a map anyway so not too worried, just start a new round
+				warn(err)
+				continue
+			end
+		end
+	end)
+end
+
+-- Creates the game values to be replicated to all clients to tell them the current status of the game
+function MainGameLoop:createGameValues()
+	local gameValues = Instance.new("Folder", ReplicatedStorage)
+	gameValues.Name = "GameValues"
+	local gameStatus = Instance.new("StringValue", gameValues)
+	gameStatus.Name = "GameStatus"
+	local roundType = Instance.new("StringValue", gameValues)
+	roundType.Name = "RoundType"
+	self.gameStatus = gameStatus
+	self.roundType = roundType
+end
+
+-- When the player clicks spawn on their kit selection screen, spawn them into the game with their chosen kit
+function MainGameLoop:spawnPlayer(player, chosenKit)
+	-- Check if they are allowed to use the kit they chose
+	if self.currentMode then
+		self.currentMode:spawnPlayer(player)
+	end
+end
+
+spawnPlayerEvent.OnServerEvent:Connect(function(player, chosenKit)
+	MainGameLoop:spawnPlayer(player, chosenKit)
+end)
+
+return MainGameLoop

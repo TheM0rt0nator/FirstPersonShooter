f2f9fc05e508a3f5779f352be9b763fd2c360913
Arthur Morton <a.smorton5@gmail.com>
2022-04-18 16:50:02 +0100

    WeaponKits module

--- src/ReplicatedStorage/SharedScripts/WeaponHandler/WeaponKits.lua
+++ src/ReplicatedStorage/SharedScripts/WeaponHandler/WeaponKits.lua
@@ -1,11 +1,34 @@
+-- Lists the weapon kits and what each kit contains
+
 return {
 	Assault = {
 		Weapons = {
 			Primary = "M4A1";
-			Secondary = "DesertEagle";
+			Secondary = "Desert Eagle";
 		};
 		Equipment = {
 			"Flashbang";
 		};
-	}
+		layoutOrder = 1;
+	};
+	Recon = {
+		Weapons = {
+			Primary = "Intervention";
+			Secondary = "M1911A1";
+		};
+		Equipment = {
+			"M67";
+		};
+		layoutOrder = 2;
+	};
+	Support = {
+		Weapons = {
+			Primary = "AK47";
+			Secondary = "RPG";
+		};
+		Equipment = {
+			"Smoke Grenade";
+		};
+		layoutOrder = 3;
+	};
 }

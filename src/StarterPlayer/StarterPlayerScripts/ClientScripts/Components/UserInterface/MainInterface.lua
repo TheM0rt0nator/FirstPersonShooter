4e34f07f2933eac973e24259dc01ff6122f0ab3b
Arthur Morton <a.smorton5@gmail.com>
2022-04-18 16:48:10 +0100

    Main UI system setup

---
+++ src/StarterPlayer/StarterPlayerScripts/ClientScripts/Components/UserInterface/MainInterface.lua
@@ -0,0 +1,70 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local loadModule, getDataStream = table.unpack(require(ReplicatedStorage.Framework))
+
+local SetInterfaceState = getDataStream("SetInterfaceState", "BindableEvent")
+
+local Roact = loadModule("Roact")
+local Maid = loadModule("Maid")
+
+local MainInterface = Roact.Component:extend("MainInterface")
+
+local Components = {
+	KitSelection = loadModule("KitSelection");
+}
+
+local InterfaceStates = {
+	kitSelection = {
+		"KitSelection";
+	};
+}
+
+function MainInterface:init()
+	self.maid = Maid.new()
+	self.currentState = nil
+	self.visibilityBindings = {}
+	self.setVisibleBindings = {}
+	-- Create a binding for each component, to tell it whether to be visible or not
+	for componentName, _ in pairs(Components) do
+		self.visibilityBindings[componentName], self.setVisibleBindings[componentName] = Roact.createBinding(false)
+	end
+
+	-- When the setInterfaceState Bindable is fired, set the state of the interface and set the relevant components visibility binding to true/false
+	self.maid:GiveTask(SetInterfaceState.Event:Connect(function(state)
+		if state and InterfaceStates[state] and self.currentState ~= state then
+			self:setState(state)
+		end
+	end))
+
+	self:setState("kitSelection")
+end
+
+function MainInterface:setState(state)
+	self.currentState = state
+	local stateComponents = InterfaceStates[state]
+	-- Set the components in this state to visible = true and the rest to false
+	for componentName, _ in pairs(Components) do
+		if not table.find(stateComponents, componentName) and self.visibilityBindings[componentName] and self.setVisibleBindings[componentName] then
+			self.setVisibleBindings[componentName](false)
+		else
+			self.setVisibleBindings[componentName](true)
+		end
+	end
+end
+
+function MainInterface:render()
+	local children = {}
+
+	for componentName, component in pairs(Components) do
+		children[componentName] = Roact.createElement(component, {
+			visible = self.visibilityBindings[componentName]
+		})
+	end
+
+	-- Create a screen Gui for the main interface, and put everything else inside this screen gui
+	return Roact.createElement("ScreenGui", {
+		Name = "MainInterface";
+	}, children)
+end
+
+return MainInterface
---
+++ src/StarterPlayer/StarterPlayerScripts/ClientScripts/Components/UserInterface/init.lua
@@ -0,0 +1,18 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local loadModule = table.unpack(require(ReplicatedStorage.Framework))
+
+local Player = Players.LocalPlayer
+
+local Roact = loadModule("Roact")
+local MainInterface = loadModule("MainInterface")
+
+local UserInterface = Roact.createElement(MainInterface)
+
+-- When this module runs, mount the main interface
+function UserInterface:initiate()
+	--Roact.mount(UserInterface, Player:WaitForChild("PlayerGui"))
+end
+
+return UserInterface

d6b744b5416c5d063cc3e4a47f4926f40a23e607
Arthur Morton <a.smorton5@gmail.com>
2022-04-19 00:31:44 +0100

    Keybinds Module

---
+++ src/ReplicatedStorage/SharedScripts/Keybinds.lua
@@ -0,0 +1,26 @@
+return {
+	Fire = {
+		Enum.UserInputType.MouseButton1;
+		Enum.KeyCode.ButtonR2;
+	};
+	Aim = {
+		Enum.UserInputType.MouseButton2;
+		Enum.KeyCode.ButtonL2;
+	};
+	Sprint = {
+		Enum.KeyCode.LeftShift;
+		Enum.KeyCode.ButtonL3;
+	};
+	Crouch = {
+		Enum.KeyCode.C;
+		Enum.KeyCode.ButtonB;
+	};
+	Reload = {
+		Enum.KeyCode.R;
+		Enum.KeyCode.ButtonX;
+	};
+	UseEquipment = {
+		Enum.KeyCode.G;
+		Enum.KeyCode.ButtonR1;
+	};
+}

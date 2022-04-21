7a29036355f38b4346d8060a99fbc18ba2641344
Arthur Morton <a.smorton5@gmail.com>
2022-04-21 02:00:53 +0100

    Disable default leaderboard

--- src/StarterPlayer/StarterCharacterScripts/InitialCamera.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/InitialCamera.client.lua
@@ -1,5 +1,10 @@
+local StarterGui = game:GetService("StarterGui")
+
 -- When a player gets a new character, set their camera to the kit selection cam
 local Camera = workspace.CurrentCamera
+
+-- Disable built in leaderboard
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
 
 --[[Camera.CameraType = Enum.CameraType.Scriptable
 Camera.CFrame = CFrame.new(0, 0, 0)]]

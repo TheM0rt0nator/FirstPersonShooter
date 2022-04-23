0590e86c2513e9548a090c6ad829d2567bfad31c
Arthur Morton <a.smorton5@gmail.com>
2022-04-23 19:16:33 +0100

    Set camera only on first join

--- src/StarterPlayer/StarterCharacterScripts/InitialCamera.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/InitialCamera.client.lua
@@ -3,8 +3,13 @@ local StarterGui = game:GetService("StarterGui")
 -- When a player gets a new character, set their camera to the kit selection cam
 local camera = workspace.CurrentCamera
 
--- Disable built in leaderboard
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+local isFistJoin = true
 
-camera.CameraType = Enum.CameraType.Scriptable
-camera.CFrame = CFrame.new(0, 0, 0)
+if isFistJoin then
+	isFistJoin = false
+	-- Disable built in leaderboard
+	StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+
+	camera.CameraType = Enum.CameraType.Scriptable
+	camera.CFrame = CFrame.new(0, 0, 0)
+end
